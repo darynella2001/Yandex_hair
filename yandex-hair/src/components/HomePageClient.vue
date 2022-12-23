44ed5560a544eb572ebed83eea54bdd4c07263a5
John Wick <sarapovaaniuta@gmail.com>
2022-12-23 20:54:14 +0200

    add 2 components

---
+++ yandex-hair/src/components/HomePageClient.vue
@@ -0,0 +1,13 @@
+<template>
+ <div> <h3>Client</h3></div>
+</template>
+
+<script>
+export default {
+  name: "HomePageClient"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ yandex-hair/src/components/HomePageHairdresser.vue
@@ -0,0 +1,13 @@
+<template>
+  <div> <h3>Hairdresser</h3></div>
+</template>
+
+<script>
+export default {
+  name: "HomePageHairdresser"
+}
+</script>
+
+<style scoped>
+
+</style>
